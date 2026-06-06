2816d039924dfe468e48fd7327ea0ecdcda41939
Bandarapu Goutham Kumar Goud <gouthambandarapu8@gmail.com>
2026-06-06 20:02:25 +0530

    Add pages barrel export

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import SubjectsPage from './pages/SubjectsPage.jsx'
+import RoadmapPage from './pages/RoadmapPage.jsx'
+import GamePage from './pages/GamePage.jsx'
+import TrendsPage from './pages/TrendsPage.jsx'
+import NotesPage from './pages/NotesPage.jsx'
+import PinkyAIPage from './pages/PinkyAIPage.jsx'
+import DashboardPage from './pages/DashboardPage.jsx'
+
+export {
+  SubjectsPage,
+  RoadmapPage,
+  GamePage,
+  TrendsPage,
+  NotesPage,
+  PinkyAIPage,
+  DashboardPage
+}
